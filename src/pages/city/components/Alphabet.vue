5d34ae34c84f0447700082b72fded54a7e2e8c5c
zz1995zz <913980996@qq.com>
2019-05-06 23:13:29 +0800

    列表切换优化

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -24,8 +24,13 @@ props:{
 },
 data (){
   return {
-    touchStatus:false
+    touchStatus:false,
+    startY:0,
+    timer:null
   }
+},
+updated (){
+  this.startY=this.$refs['A'][0].offsetTop
 },
 computed:{
   letters (){
@@ -45,12 +50,17 @@ methods:{
   },
   handleTouchMove (e){
     if(this.touchStatus){
-      const startY=this.$refs['A'][0].offsetTop
-      const touchY=e.touches[0].clientY-79
-      const index=Math.floor((touchY-startY)/20)
-      if(index >=0 && index < this.letters.length){
-        this.$emit('change',this.letters[index])
+      // 事件节流
+      if(this.timer){
+        clearTimeout(this.timer)
       }
+      this.timer=setTimeout(() => {
+        const touchY=e.touches[0].clientY-79
+        const index=Math.floor((touchY-this.startY)/20)
+        if(index >=0 && index < this.letters.length){
+          this.$emit('change',this.letters[index])
+      }
+    },20)
     }
   },
   handleTouchEnd (){
