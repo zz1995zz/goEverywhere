874fa0e6553aa129477a7ecb5f221722ed789327
zz1995zz <913980996@qq.com>
2019-05-08 10:51:02 +0800

    使用递归组件实现详情页分级列表

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,58 @@
   <div>
   	<detail-header></detail-header>
     <detail-banner></detail-banner>
-    <div class='scroll-bottom'></div>
+    <div class='scroll-bottom'>
+      <detail-list :lists='lists'></detail-list>
+    </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components:{
     DetailBanner,
     DetailHeader,
+    DetailList,
+  },
+  data (){
+    return {
+      lists:[
+      {
+        title:'成人票',
+        children:[
+        {
+          title:'三馆联合优惠票'
+        },
+        {
+          title:'五馆联合优惠票'
+        },
+        {
+          title:'情侣优惠票',
+          children:[
+          {
+            title:'一日限定情侣套餐'
+          },
+          {
+            title:'多日限定情侣套餐'
+          }
+          ]
+        }
+        ]
+      },
+      {
+        title:'学生票'
+      },
+      {
+        title:'儿童票'
+      },
+      {
+        title:'特惠票'
+      }
+      ]
+    }
   }
 };
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div 
+    class="item border-bottom"
+    v-for='(item,index) of lists'
+    :key='index'
+    >
+      <div class="item-title">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if='item.children'>
+        <detail-list :lists='item.children'></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props:{
+    lists:Array
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+.item
+  line-height:.8rem
+  font-size:.32rem
+  padding-left:.2rem
+  .item-title-icon
+    position:relative
+    display:inline-block
+    left:.02rem
+    top:.06rem
+    width: .36rem
+    height: .36rem
+    background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+    background-size: .4rem 3rem
+</style>
